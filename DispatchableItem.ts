754ab1398556d01f96d37e4beb2ed1c4608365df
agusgroh <agusgroh@gmail.com>
2024-01-31 12:11:31 -0300

    Delta

---
+++ DispatchableItem.ts
@@ -0,0 +1,68 @@
+import { FingerprintPackage } from '../WfpProvider/FingerprintPackage';
+import FormData from 'form-data';
+import { SbomMode } from '../ScannerTypes';
+
+export class DispatchableItem {
+  private readonly form: FormData;
+
+  private errorCounter: number;
+
+  private fingerprintPackage: FingerprintPackage;
+
+  private engineFlags: number;
+
+  private sbom: string;
+
+  private sbomMode: SbomMode;
+
+  private _uuid: string;
+  constructor() {
+    this.errorCounter = 0;
+    this.form = new FormData();
+  }
+
+  public getForm(): FormData {
+    this.form.append('file', Buffer.from(this.fingerprintPackage.getContent()), 'data.wfp');
+    if(this.engineFlags) this.form.append('flags', this.engineFlags);
+
+    if(this.sbomMode && this.sbom) {
+      this.form.append('assets', this.sbom);
+      this.form.append('type', this.sbomMode);
+    }
+
+    return this.form;
+  }
+
+  public increaseErrorCounter() {
+    this.errorCounter += 1;
+  }
+
+  public getErrorCounter() {
+    return this.errorCounter;
+  }
+
+  public setFingerprintPackage(fingerprintPackage: FingerprintPackage) {
+    this.fingerprintPackage = fingerprintPackage;
+  }
+
+  public getFingerprintPackage(): FingerprintPackage {
+    return this.fingerprintPackage;
+  }
+
+  public setEngineFlags(engineFlags: number) {
+    this.engineFlags = engineFlags;
+  }
+
+  public setSbom(sbom: string, sbomMode: SbomMode) {
+    this.sbom = sbom;
+    this.sbomMode = sbomMode;
+  }
+
+  public get uuid(): string {
+    return this._uuid;
+  }
+
+  public set uuid(uuid: string) {
+    this._uuid = uuid;
+  }
+}
